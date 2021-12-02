284095994198a90f539b711409fba4f85c137fba
Austin Albrecht <austin.aa@me.com>
2021-12-02 08:45:58 +0100

    Update day01 to avoid conflicts with day02

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,31 @@
 fun main() {
+
+    fun part1(input: List<String>): Int {
+        val data = mapToInts(input)
+        var count = 0
+
+        for (i in 1 until data.size) {
+            val curr = data[i]
+            val prev = data[i-1]
+            if (curr > prev) count++
+        }
+
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        val data = mapToInts(input)
+        var count = 0
+
+        for (i in 3 until data.size) {
+            val curr = data[i] + data[i - 1] + data[i - 2]
+            val prev = data[i - 1] + data[i - 2] + data[i - 3]
+            if (curr > prev) count++
+        }
+
+        return count
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
@@ -9,31 +36,6 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    val data = mapToInts(input)
-    var count = 0
-
-    for (i in 1 until data.size) {
-        val curr = data[i]
-        val prev = data[i-1]
-        if (curr > prev) count++
-    }
-
-    return count
-}
-
-fun part2(input: List<String>): Int {
-    val data = mapToInts(input)
-    var count = 0
-
-    for (i in 3 until data.size) {
-        val curr = data[i] + data[i - 1] + data[i - 2]
-        val prev = data[i - 1] + data[i - 2] + data[i - 3]
-        if (curr > prev) count++
-    }
-
-    return count
-}
 
 fun mapToInts(input: List<String>): List<Int> {
     return input.stream().map { str -> str.toInt() }.toList()
